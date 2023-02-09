4b0d3ecc7b566d61e56fde1b78f6b9dbab6ed336
Micah Morris <micah.morris42.com>
2023-02-09 16:27:44 -0700

    complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    
+    if (firstNumber >= secondNumber) {
+        highNum = firstNumber;
+        lowNum = secondNumber;
+
+    } 
+        else {
+        highNum = secondNumber;
+        lowNum = firstNumber;
+        
+    }
+   if (firstNumber < 0 || secondNumber < 0) {
+        return 'ERROR'
+   }
+   else if (typeof firstNumber != 'number' || typeof secondNumber != 'number') {
+        return 'ERROR'
+   }
+   else{
+   let sum = 0;
+    for (let i = lowNum; i <= highNum; i++) {
+        let number = i;
+        sum = sum + number;
+    }
+    return sum;}
 
 };
 
