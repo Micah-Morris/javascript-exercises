f5251e0485e86e7c5e29543667f02444c2336e3a
Micah Morris <micah.morris42.com>
2023-02-09 15:13:42 -0700

    complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeNum) 
+{
+   for (let i = 0; i < removeNum.length; i++) {
+    let number = removeNum[i];
+    let index = array.indexOf(number);
+    if (index >= 0) {
+         array.splice(index, 1);
+    }
+   
+   }
+    return array;   
 
 };
 
