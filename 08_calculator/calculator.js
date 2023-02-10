cea74dff7c64ffa9aef0675fab8dc5e95992bcad
Micah Morris <micah.morris42.com>
2023-02-09 21:35:50 -0700

    complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+	let answer = numOne + numTwo;
+  return answer;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	let answer = numOne - numTwo;
+  return answer;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current,);
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
+  };
 
 // Do not edit below this line
 module.exports = {
