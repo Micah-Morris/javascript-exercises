98c5b7ba0d8ecd64ab44c2c84e7d23863aab3bc2
Micah Morris <micah.morris42.com>
2023-02-09 19:59:26 -0700

    complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let cel = (far - 32) * (5/9);
+  let celTemp = Math.round(cel * 10) /10
+  return celTemp
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let far = (cel) * (9/5) + 32;
+  let farTemp = Math.round(far * 10) /10
+  return farTemp
 };
 
 // Do not edit below this line
